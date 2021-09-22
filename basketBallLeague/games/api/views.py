7a1a566c6411d179d69e793167ffe67c2eb8a46a
tharuka-amaraweera <tharuka95amaraweera@gmail.com>
2021-09-22 21:41:30 +0530

    removed unwanted files

--- basketBallLeague/games/api/views.py
+++ basketBallLeague/games/api/views.py
@@ -8,19 +8,13 @@ from rest_framework.exceptions import ValidationError
 
 
 # Functionality: get all games in sorted order to reflect who won & competition progress
-# to access the details user must have logged into the system
-# details can be accessible only by an admin user
-
+# everyone can access this api call
 class GameOrderedList(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
+
     queryset = Game.objects.all()
     serializer_class = GameListSerializer
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['finalscore']
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return Game.objects.all()
-        else:
-            raise ValidationError(
-                "Only the admin user can view the game details!")
+        return Game.objects.all()
